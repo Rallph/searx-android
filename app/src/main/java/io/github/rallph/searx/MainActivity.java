fb3a55926cce219902fee7fa0ce00e35cc27eeb9
Rallph <rallph@protonmail.com>
2018-12-01 17:50:56 -0600

    Display formatted results

--- app/src/main/java/io/github/rallph/searx/MainActivity.java
+++ app/src/main/java/io/github/rallph/searx/MainActivity.java
@@ -16,8 +16,14 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     SearchView mSearchBox;
     TextView mResultsView;
 
-    final String TAG = "Main Activity";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,36 +44,22 @@ public class MainActivity extends AppCompatActivity {
         mSearchBox = findViewById(R.id.search_box);
         mResultsView = findViewById(R.id.results_box);
 
+
+
         mSearchBox.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
             @Override
             public boolean onQueryTextSubmit(String query) {
 
                 String queryUrl = url + "?q=" + query + "&format=json";
-                Log.d(TAG,"Query is " + query);
+                Log.d(TAG, "Query is " + query);
                 Log.d(TAG, "Query URL is " + queryUrl);
 
-                Toast.makeText(getApplicationContext(), "Submitted", Toast.LENGTH_SHORT).show();
-
-
-                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, queryUrl, null,new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-
-                        Log.d(TAG, "Response received");
-                        mResultsView.append(response.toString());
-
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();;
-                    }
-                });
-
-                queue.add(request);
+                Toast.makeText(getApplicationContext(), "Searched", Toast.LENGTH_SHORT).show();
+                getResults(queryUrl);
                 return false;
             }
+
 
             @Override
             public boolean onQueryTextChange(String newText) {
@@ -75,5 +67,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    public void getResults(String queryUrl) {
+
+
+
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, queryUrl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Toast.makeText(getApplicationContext(), "response received", Toast.LENGTH_SHORT).show();
+                try {
+
+                    JSONArray results = response.getJSONArray("results");
+
+                    for (int i = 0; i < results.length(); i++) {
+
+                        JSONObject result = (JSONObject) results.get(i);
+
+                        Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
+
+                        String entry = result.getString("title") + "\n" + result.getString("url") + "\n";
+
+                        if (result.has("content")) {
+                            entry += result.getString("content");
+                        }
+
+                        entry += "\n\n";
+
+                        mResultsView.append(entry);
+
+
+
+                    }
+
+                } catch (JSONException e) {
+
+                    Toast.makeText(getApplicationContext(), "shit fucked up in the json", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "shit fucked up in volley", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(request);
     }
 }
--- app/src/main/java/io/github/rallph/searx/SearchResult.java
+++ app/src/main/java/io/github/rallph/searx/SearchResult.java
@@ -1,6 +1,122 @@
 package io.github.rallph.searx;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class SearchResult {
 
+    @SerializedName("engine")
+    private String mEngine;
+    @SerializedName("category")
+    private String mCategory;
+    @SerializedName("engines")
+    private List<String> mEngines;
+    @SerializedName("title")
+    private String mTitle;
+    @SerializedName("url")
+    private String mUrl;
+    @SerializedName("positions")
+    private List<Integer> mPositions;
+    @SerializedName("parsed_url")
+    private List<String> mParsedUrl;
+    @SerializedName("content")
+    private String mContent;
+    @SerializedName("pretty_url")
+    private String mPrettyUrl;
+    @SerializedName("score")
+    private int mScore;
 
+    public  SearchResult(String engine, String category, List<String> engines, String title, String url, List<Integer> positions, List<String> parsed_url, String content, String pretty_url, int score) {
+        mEngine = engine;
+        mCategory = category;
+        mEngines = engines;
+        mTitle = title;
+        mUrl = url;
+        mPositions = positions;
+        mParsedUrl = parsed_url;
+        mContent = content;
+        mPrettyUrl = pretty_url;
+        mScore = score;
+    }
+
+    public String getmEngine() {
+        return mEngine;
+    }
+
+    public void setmEngine(String mEngine) {
+        this.mEngine = mEngine;
+    }
+
+    public String getmCategory() {
+        return mCategory;
+    }
+
+    public void setmCategory(String mCategory) {
+        this.mCategory = mCategory;
+    }
+
+    public List<String> getmEngines() {
+        return mEngines;
+    }
+
+    public void setmEngines(List<String> mEngines) {
+        this.mEngines = mEngines;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    public List<Integer> getmPositions() {
+        return mPositions;
+    }
+
+    public void setmPositions(List<Integer> mPositions) {
+        this.mPositions = mPositions;
+    }
+
+    public List<String> getmParsedUrl() {
+        return mParsedUrl;
+    }
+
+    public void setmParsedUrl(List<String> mParsedUrl) {
+        this.mParsedUrl = mParsedUrl;
+    }
+
+    public String getmContent() {
+        return mContent;
+    }
+
+    public void setmContent(String mContent) {
+        this.mContent = mContent;
+    }
+
+    public String getmPrettyUrl() {
+        return mPrettyUrl;
+    }
+
+    public void setmPrettyUrl(String mPrettyUrl) {
+        this.mPrettyUrl = mPrettyUrl;
+    }
+
+    public int getmScore() {
+        return mScore;
+    }
+
+    public void setmScore(int mScore) {
+        this.mScore = mScore;
+    }
 }
